d83bbfdc87649689c859e93a64809857e5d5d2fc
rip_asura <dhyannj12@gmail.com>
2026-03-20 19:39:29 +0530

    Create Locker-room.py

---
+++ Problems solving chalenges/the locker room challenge/Locker-room.py
@@ -0,0 +1,9 @@
+i = 0
+open_lockers = []
+
+while i**2 <= 1000:
+    open_lockers.append(i**2)
+    i+=1
+
+print("The open lockers are: ", open_lockers)
+print("\nThis is because square number have an odd numbers of factors as one of thier factors are multiplied twice.")
